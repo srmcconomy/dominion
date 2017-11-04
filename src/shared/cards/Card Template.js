20fb646819fdce57de4890bd6b6e6db40e242df4
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-11-04 15:28:54 -0400

    Changes from QA

--- src/shared/cards/Card Template.js
+++ src/shared/cards/Card Template.js
@@ -1,8 +1,7 @@
 import Card from 'cards/Card';
-import { Set } from 'immutable';
 
 export default class CardName extends Card {
-  name = 'Card Name';
+  static title = 'Card Name';
   cost = 4;
   VP = 3;
   coin = 2;
@@ -16,9 +15,9 @@ export default class CardName extends Card {
 
     // Trash card(s) in Hand, is it game.currentPlayer, or just Player?
     const cards = await game.currentPlayer.selectCards(min, max, predicate);
-    for card in cards {
+    cards.forEach(card => {
       player.trash(card);
-    }
+    })
 
     // Trash this card
     player.trash(this);
@@ -31,42 +30,46 @@ export default class CardName extends Card {
 
     // Draw multiple cards and do something if(), basically emulating library
     const cards = player.setAside(min, max);
-    for card in cards {
-      if card.types.has('Action') {
-        await choice = player.chooseOption(['hand', 'discard']);
-        if (choice == 'hard') {
+    cards.forEach(card => {
+      if (card.types.has('Action')) {
+        const choice = await player.chooseOption(['hand', 'discard']);
+        if (choice === 'hard') {
           player.moveCard(card, player.setAside, player.hand);
+        } else {
+          player.moveCard(card, player.setAside, player.discard);
         }
-        else {player.moveCard(card, player.setAside, player.discard);}
       }
-    }
+    });
+    player.unsetAside();
+
 
     // Oponents draw card(s) and you do something , basically thief
-    await game.forEachOtherPlayer(async player => {
-      cards = player.deck[end-x:end];
-      await card = player.selectCards(1, 1, card => card.types.has(['Treasure'], cards);
-      player.trash(card);
+    await game.forEachOtherPlayer(async otherPlayers => {
+      const cards = otherPlayers.setAside(x,x);
+      const card = await otherPlayers.selectCards(1, 1, card => card.types.has(['Treasure'], cards);
+      otherPlayers.trash(card);
+      otherPlayers.unsetAside();
     });
 
     // Other players discard X
-    await game.forEachOtherPlayer(async player => {
-      if (player.hand.size > 0) {
-        const cards = await player.selectCards(X, 'Select card(s) to discard');
-        cards.forEach(card => player.discard(card));
+    await game.forEachOtherPlayer(async otherPlayers => {
+      if (otherPlayers.hand.size > 0) {
+        const cards = await otherPlayers.selectCards(X, 'Select card(s) to discard');
+        cards.forEach(card => otherPlayers.discard(card));
       }
     });
 
     // Other players discard down to X
-    await game.forEachOtherPlayer(async player => {
-      if (player.hand.size > X) {
-        const cards = await player.selectCards(player.hand.count - X, 'Select card(s) to discard');
-        cards.forEach(card => player.discard(card));
+    await game.forEachOtherPlayer(async otherPlayers => {
+      if (otherPlayers.hand.size > X) {
+        const cards = await otherPlayers.selectCards(player.hand.count - X, 'Select card(s) to discard');
+        cards.forEach(card => otherPlayers.discard(card));
       }
     });
 
     // Gain a Card
-    const supply = await player.chooseSupplyWhere(supply => (supply.last().cost <= 5 && supply.last.types.has('Treasure')));
-    player.gain(supply.last().name);
+    const supply = await player.chooseSupplyWhere(supply => (supply.last().cost <= 5 && supply.last().types.has('Treasure')));
+    player.gain(supply.last());
 
 
   }
