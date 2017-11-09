112d5c52abeccebd983bc16a656c65967b91491c
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-11-08 21:56:14 -0500

    Sanity Checks on Lurker

--- src/server/cards/base/index.js
+++ src/server/cards/base/index.js
@@ -1,46 +1,4 @@
-// import Card from 'cards/Card';
-// import * as Cards from './';
-
-// Object.keys(Cards).forEach(title => Card.classes.set(title, Cards[title]));
-
 import Card from 'cards/Card';
-import Bureaucrat from './bureaucrat';
-import Cellar from './cellar';
-import Chapel from './chapel';
-import CouncilRoom from './councilRoom';
-import Festival from './festival';
-import Gardens from './gardens';
-import Laboratory from './laboratory';
-import Library from './library';
-import Market from './market';
-import Militia from './militia';
-import Mine from './mine';
-import Moat from './moat';
-import MoneyLender from './moneyLender';
-import Remodel from './remodel';
-import Smithy from './smithy';
-import ThroneRoom from './throneRoom';
-import Village from './village';
-import Witch from './witch';
-import Workshop from './workshop';
+import * as Cards from './';
 
-Card.classes.set('Bureaucrat', Bureaucrat);
-Card.classes.set('Cellar', Cellar);
-Card.classes.set('Chapel', Chapel);
-Card.classes.set('CouncilRoom', CouncilRoom);
-Card.classes.set('Festival', Festival);
-Card.classes.set('Gardens', Gardens);
-Card.classes.set('Laboratory', Laboratory);
-Card.classes.set('Library', Library);
-Card.classes.set('Market', Market);
-Card.classes.set('Militia', Militia);
-Card.classes.set('Mine', Mine);
-Card.classes.set('Moat', Moat);
-Card.classes.set('MoneyLender', MoneyLender);
-Card.classes.set('Remodel', Remodel);
-Card.classes.set('Smithy', Smithy);
-Card.classes.set('ThroneRoom', ThroneRoom);
-Card.classes.set('Village', Village);
-Card.classes.set('Witch', Witch);
-Card.classes.set('Workshop', Workshop);
-
+Object.keys(Cards).forEach(title => Card.classes.set(title, Cards[title]));
--- src/server/cards/intrigue/Lurker.js
+++ src/server/cards/intrigue/Lurker.js
@@ -12,14 +12,22 @@ export default class Lurker extends Card {
     switch (choice) {
       case 0: {
         const [supply] = await player.selectSupplies({ min: 1, max: 1, predicate: s => s.cards.size > 0 && s.cards.last().types.has('Action'), message: 'Choose an action to trash' });
-        const [card] = player.moveCard(supply.cards, player.game.trash);
-        await card.onTrash();
+        if (supply) {
+          const [card] = player.moveCard(supply.cards, player.game.trash);
+          if (card) {
+            await card.onTrash();
+          }
+        }
         break;
       }
       case 1: {
-        const [card] = await player.selectCards({ min: 1, max: 1, pile: player.game.trash.filter(c => c.types.has('Action')), message: 'Choose an action to gain' });
-        player.moveCard(card, player.game.trash, player.discardPile);
-        await card.onGain();
+        if (player.game.trash.size > 0) {
+          const [card] = await player.selectCards({ min: 1, max: 1, pile: player.game.trash.filter(c => c.types.has('Action')), message: 'Choose an action to gain' });
+          if (card) {
+            player.moveCard(card, player.game.trash, player.discardPile);
+            await card.onGain();
+          }
+        }
         break;
       }
       default:
