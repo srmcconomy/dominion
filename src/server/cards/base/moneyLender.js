87e22a4c29d55f2d4527417ca8bb78d3b888da73
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-11-09 14:10:27 -0500

    Fixed case in file name

---
+++ src/server/cards/base/moneyLender.js
@@ -0,0 +1,14 @@
+import Card from 'cards/Card';
+import { Set } from 'immutable';
+
+export default class MoneyLender extends Card {
+  static cost = 4;
+  static types = new Set(['Action']);
+  async onPlay(player) {
+    const [card] = await player.selectCards({ min: 0, max: 1, predicate: card => card.name === 'Copper', message: 'Choose a Copper card to trash' })
+    if (card) {
+    	player.trash(card);
+    	player.money += 3;
+    }
+  }
+}
