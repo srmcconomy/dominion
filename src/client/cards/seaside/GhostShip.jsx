fdff8075074b5d1465a5d3672b80a7e20c6ed062
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-12-03 16:12:50 -0500

    Explorer, Ghost Ship

---
+++ src/client/cards/seaside/GhostShip.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from 'cards/Card';
+import Coin from 'components/Coin';
+
+export default class GhostShip extends Card {
+  static description = (<div>
+  	<div><strong>+2 Cards</strong></div>
+  	<div>Each other player with 4 or more cards in hand puts cards from their hand onto their deck until they have 3 cards in hand.</div>
+  	</div>);
+  static cost = <Coin>5</Coin>
+  static types = ['Action', 'Attack'];
+}
+
+Card.classes.set('GhostShip', GhostShip);
