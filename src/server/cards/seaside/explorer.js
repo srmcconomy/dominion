8527db00c953e02b56c071bd8c7c6b50e78451d3
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-12-03 16:12:50 -0500

    Explorer, Ghost Ship

---
+++ src/server/cards/seaside/explorer.js
@@ -0,0 +1,14 @@
+import Card from 'cards/Card';
+
+export default class Explorer extends Card {
+  static cost = 5;
+  static types = new Set(['Action']);
+  async onPlay(player) {
+    const [card] = await player.selectCards({min:0, max:1, predicate: c=> c.title == 'Province', message:'You may reveal a Province to gain a Gold, else gain a silver'});
+    if (card) {
+      await player.gain('Gold', player.hand);
+    } else {
+      await player.gain('Silver', player.hand);
+    }
+  }
+}
