15714f790e0ed02d3bbec3e141c391754968820b
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-12-03 23:22:15 -0500

    Fix client discrpitions

--- src/client/cards/seaside/Navigator.jsx
+++ src/client/cards/seaside/Navigator.jsx
@@ -3,10 +3,10 @@ import Card from 'cards/Card';
 import Coin from 'components/Coin';
 
 export default class Navigator extends Card {
-  static description = <div>
-  <p>+<Coin>2</Coin></p>
-  <p>Look at the top 5 cards of your deck. Either discard them all, or put them back in any order.</p>
-  </div>
+  static description = (<div>
+  <div>+<Coin>2</Coin></div>
+  <div>Look at the top 5 cards of your deck. Either discard them all, or put them back in any order.</div>
+  </div>);
   static cost = <Coin>4</Coin>
   static types = ['Action'];
 }
--- src/client/cards/seaside/SeaHag.jsx
+++ src/client/cards/seaside/SeaHag.jsx
@@ -4,7 +4,7 @@ import Coin from 'components/Coin';
 
 export default class SeaHag extends Card {
   static title = 'Sea Hag'
-  static description = 'Each other player discards the top card of their deck, then gains a Curse onto their deck.';
+  static description = (<div>Each other player discards the top card of their deck, then gains a Curse onto their deck.</div>);
   static cost = <Coin>4</Coin>
   static types = ['Action','Attack'];
 }
