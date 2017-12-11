48f74c7767968c5684d405650f7fa63e6c6861f7
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-12-10 23:15:33 -0500

    Make Throne Room work on durations

--- src/server/cards/base/throneRoom.js
+++ src/server/cards/base/throneRoom.js
@@ -6,11 +6,23 @@ export default class ThroneRoom extends Card {
   async onPlay(player) {
     const [card] = await player.selectCards({ min: 0, max: 1, predicate: card => card.types.has('Action'), message: 'Choose an Action card to play twice' });
     if (card) {
-      await player.play(card);
-      await player.pickUp(card);
-      if (card) {
-        await player.play(card);
+      this.card = card
+      await player.play(this.card);
+      if (player.playArea.hasID(this.card.id)) {
+        await player.pickUp(this.card);
+        await player.play(this.card);
+        this.ignoreCleanUp = this.card.ignoreCleanUp;
       }
+    }
+  }
+
+  async onTurnStart(player) {
+    if (player.playArea.hasID(this.card.id)) {
+      await this.card.onTurnStart(player);
+      this.ignoreCleanUp = this.card.ignoreCleanUp;
+
+    } else {
+      this.ignoreCleanUp = false;
     }
   }
 }
