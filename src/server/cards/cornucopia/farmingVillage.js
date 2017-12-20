757518ab4eb33056c0e05410da196e36c39bcffc
Ian Grzegorczyk <ian_grzegorczyk@hotmail.com>
2017-12-20 16:21:26 -0500

    Farming Village, Fortune Teller, Hunting Party

---
+++ src/server/cards/cornucopia/farmingVillage.js
@@ -0,0 +1,22 @@
+import Card from 'cards/Card';
+import Pile from 'utils/Pile';
+
+export default class FarmingVillage extends Card {
+  static cost = { coin: 4 };
+  static types = new Set(['Action']);
+  async onPlay(player) {
+    player.actions += 2;
+    const aside = new Pile();
+    while (player.deck.size + player.discardPile.size > 0) {
+      const [card] = await player.draw(1, false);
+      player.game.log(`${player.name} reveals ${card.title}`);
+      if (card.types.has('Action') || card.types.has('Treasure')) {
+        player.hand.push(card);
+        break;
+      } else {
+        aside.push(card);
+      }
+    }
+    await aside.asyncForEach(card => player.discard(card));
+  }
+}
